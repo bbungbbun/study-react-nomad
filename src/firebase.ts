875a75e896f2816c34265183e85550eacef91ced
bbungbbun <tnqkrdbsl@naver.com>
2023-11-02 15:08:54 +0900

    Firebase 세팅

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAakDKJhTRRO3Pid5OOVMKT0pEF7Tdsias",
+  authDomain: "nwitter-reloaded-c27fc.firebaseapp.com",
+  projectId: "nwitter-reloaded-c27fc",
+  storageBucket: "nwitter-reloaded-c27fc.appspot.com",
+  messagingSenderId: "262589880132",
+  appId: "1:262589880132:web:9aec1ba43c06e9abd1a911"
+};
+
+const app = initializeApp(firebaseConfig);
